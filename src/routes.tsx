d954acd9703eacc27e8162e9f66909da4df8b27d
Nahom <nahomamare52@gmail.com>
2025-02-03 00:50:56 +0300

    update file names

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,26 +1,26 @@
-import { createBrowserRouter } from 'react-router';
-import Home from './pages/home';
-import Login from './pages/login';
-import Register from './pages/register';
-import Dashboard from './pages/dashboard';
+import { createBrowserRouter } from "react-router";
+import Home from "./pages/home";
+import Login from "./pages/LoginChange";
+import Register from "./pages/RegisterChange";
+import Dashboard from "./pages/dashboard";
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Home />,
   },
   {
-    path: '/login',
+    path: "/login",
     element: <Login />,
   },
 
   {
-    path: '/register',
+    path: "/register",
     element: <Register />,
   },
   {
-    path: '/dashboard',
-    element:<Dashboard/>
-  }
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
 ]);
 
 export default router;
