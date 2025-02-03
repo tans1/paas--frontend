4cf8e90d3cc6a1cb7fd6f2d9106fe8757aa6472e
Nahom <nahomamare52@gmail.com>
2025-02-03 17:23:44 +0300

    update domain text

--- src/pages/domain.tsx
+++ src/pages/domain.tsx
@@ -8,7 +8,7 @@ const Domain = () => {
     return (
         <div className="bg-gradient-to-tr from-indigo-100 to-white min-h-dvh">
             <div className='text-3xl md:text-5xl text-center py-20'>
-                <Topic orangeTitle='Personalized Domain Name' title={''} text={'Getting your own unique domain has never been this easy'} />
+                <Topic orangeTitle='Seamless Custom Domain Integration' title={''} text={'Connect your domain seamlessly and deploy with ease.'} />
             </div>
             <div className='mx-auto p-14 bg-transparent rounded-2xl max-w-2xl text-center'>
                 <a href='/login' className='p-5 h-fit w-fit shadow-lg shadow-slate-500 text-white rounded-full bg-gradient-to-tr from-red-500 to-orange-400 justify-self-center'>Get Started Now!</a>
